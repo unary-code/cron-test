91bfcf954bc145cfae093adbe5f4077a064dcf04
unary-code <zacharykatz1618@gmail.com>
2025-10-02 00:49:51 -0500

    changed the path to relative path

--- test.py
+++ test.py
@@ -2,5 +2,6 @@
 # print("hi")
 # print(datetime.now())
 print("HEY")
-with open("/Users/davidkatz/Documents/Jupyter_Notebooks/Jobs-Notifier/testlog.log", "a") as f:
+# with open("/Users/davidkatz/Documents/Jupyter_Notebooks/Jobs-Notifier/testlog.log", "a") as f:
+with open("./testlog.log", "a") as f:
     f.write(f"Cron test: hi\n")
